109fa836fa4bf0bae59b4c66bb145fe2cdd57c30
tas7349 <92124797+tas7349@users.noreply.github.com>
2021-11-22 20:25:42 -0600

    Create textadventure.js

---
+++ U5 Conditionals/U5_6 TEXT ADVENTURE/textadventure.js
@@ -0,0 +1,34 @@
+alert ("You are a victim of a shipwreck and have been stranded on a deserted island with no human life around. You have no money and you are extremely tired and hungry. You see two routes in front of you. Choose the correct route to resuce yourself! GOOD LUCK!"); 
+
+let woody_dark_path_or_zigzag_sandy_route = prompt ("SELECT EITHER *NUMBER* (1 or 2) for adventure:  1: WOODY DARK PATH OR 2: ZIG ZAG SANDY ROUTE?");
+
+if(woody_dark_path_or_zigzag_sandy_route==1)
+
+{
+    let dark_path_or_dark_path_to_right= prompt ("MOVING ON.....SELECT EITHER *NUMBER* (1 or 2) ONCE MORE for further adventure:  1: DARK PATH ON THE LEFT OR 2: DARK PATH ON THE RIGHT?");
+    
+    if (dark_path_or_dark_path_to_right==1)
+    {
+    alert("BAD LUCK :( You entered a bear cave with a bunch of ANGRY Grizzlies. You just got mauled over by them and killed. GAME OVER! ")
+    }
+
+   else {
+  alert ("Good choice buddy, move further....");
+  let goingup_North_or_Going_East = prompt ("FINALLY, SELECT EITHER *NUMBER* (1 or 2) ONE LAST TIME: 1: UP NORTH OR 2: EAST ?");
+
+    if (goingup_North_or_Going_East==1) {
+
+    alert("SO SAD....You entered a hostile territory. A cannibalistic native tribe spots you and thinks of you as a threat to their land. They chase you down with spears and bludgen you to death. GAME OVER!");
+    }
+
+    else {
+    alert("You walk for another hour, BUT FINALLY SEE A LIGHTED VILLAGE :)) HOORAY! The 'human' villagers come to your rescue! CONGRATS, YOU ARE THE TENACIOUS PATHFINIDING WINNING HERO!")
+    }
+  }
+  
+  }
+
+
+else {
+alert("Oh NO :(( BAD CHOICE...You end up getting lost on the path and reach a steep cliff with a huge waterfall. You are TRAPPED and you fall and die. GAME OVER!")
+}
